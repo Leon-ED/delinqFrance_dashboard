87a68506acc56e548c93255035304acd7eb7fc85
Leon-ED <edmeeleon@outlook.fr>
2023-12-16 13:38:36 +0100

    NNN

--- src/get_data.py
+++ src/get_data.py
@@ -25,20 +25,20 @@ def get_data() -> bool:
     raise Exception('Une erreur a eu lieu et le fichier n\'a pas été téléchargé')
 
 
-def get_data2() -> bool:
-    '''Télécharge le fichier excel et le sauvegarde dans le dossier data'''
+# def get_data2() -> bool:
+#     '''Télécharge le fichier excel et le sauvegarde dans le dossier data'''
 
-    r = requests.get(DATA_URL2)
-    if r.status_code != 200:
-        raise Exception(f'La requête pour télécharger le fichier a échoué \n Code erreur: {r.status_code}')
+#     r = requests.get(DATA_URL2)
+#     if r.status_code != 200:
+#         raise Exception(f'La requête pour télécharger le fichier a échoué \n Code erreur: {r.status_code}')
 
-    with open(FILE_PATH, 'wb') as f:
-        f.write(r.content)
-    if os.path.isfile(FILE_PATH):
-        print(f'Le fichier {FILE_NAME} a été téléchargé avec succès, le chemin est: \n {FILE_PATH}')
-        return True
+#     with open(FILE_PATH, 'wb') as f:
+#         f.write(r.content)
+#     if os.path.isfile(FILE_PATH):
+#         print(f'Le fichier {FILE_NAME} a été téléchargé avec succès, le chemin est: \n {FILE_PATH}')
+#         return True
     
-    raise Exception('Une erreur a eu lieu et le fichier n\'a pas été téléchargé')
+#     raise Exception('Une erreur a eu lieu et le fichier n\'a pas été téléchargé')
 
 def get_global_dataframe():
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -49,5 +49,5 @@ def get_global_dataframe():
 
 if __name__ == '__main__':
     get_data()
-    get_data2()
+    # get_data2()
     print("Fin du programme")
