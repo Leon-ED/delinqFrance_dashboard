e35cf02dcbd5a6e6b4daf3a65e8a16daa267adef
Leon-ED <edmeeleon@outlook.fr>
2023-12-18 09:05:29 +0100

    Création classe utils pour les couleurs, main: affichage d'une erreur en cas de ModuleNotFoundError

---
+++ src/Utils.py
@@ -0,0 +1,34 @@
+class Colors:
+    """
+    Différentes couleurs pour les messages dans la console
+
+    Auteur
+    ----------
+    https://stackoverflow.com/a/287944
+    
+    Licence
+    ----------
+    CC BY-SA 4.0 DEED
+    https://creativecommons.org/licenses/by-sa/4.0/
+        
+    Champs
+    -------
+    HEADER, 
+    OKBLUE, 
+    OKCYAN, 
+    OKGREEN, 
+    WARNING,
+    FAIL, 
+    ENDC, 
+    BOLD, 
+    UNDERLINE
+    """
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import Utils
+
 try:
     from dash.dependencies import Input, Output
     import dash
@@ -9,10 +11,11 @@ try:
     import Graphs.CamembertFaits as CamembertFaits
     import Graphs.HistogrammeParMois as HistogrammeParMois
     import numpy as np
-except Exception as e:
-    print("Erreur lors de l'importation des modules : " + str(e))
-    print("Veuillez installer les modules requis en exécutant la commande suivante dans le dossier racine du projet:")
-    print("pip install -r requirements.txt")
+except ModuleNotFoundError as e:
+    print(f"{Utils.Colors.HEADER}L'erreur suivante est survenue lors de l'import des modules{Utils.Colors.ENDC}")
+    print(f"{Utils.Colors.FAIL}{e}{Utils.Colors.ENDC}")
+    print(f"{Utils.Colors.OKCYAN}Veuillez installer les modules requis en exécutant la commande suivante depuis le dossier racine du projet:{Utils.Colors.OKCYAN}")
+    print(f"{Utils.Colors.OKGREEN}pip install -r requirements.txt{Utils.Colors.OKGREEN}")
     exit(1)
 
 
@@ -24,98 +27,133 @@ app = dash.Dash(__name__)
 
 
 def layout(annees, mois, departements, faits, default_annee, default_mois, default_departement, default_fait):
-    app.layout = html.Main(children=[
-        html.H1(children='Delinquance en France', className='center'),
+    app.layout = html.Div(children=[
         
-        # Carte de la France
-        html.Div(id="div_map_france",children=[
-        
-        dcc.Dropdown(
-            id='map_year_dropdown',
-            options=[{'label': year, 'value': year} for year in annees],
-            value=default_annee
-        ),
-        dcc.Dropdown(
-            id='map_month_dropdown',
-            options=[{'label': month, 'value': month} for month in mois],
-            value=default_mois
-        ),
-        dcc.Graph(
-            id='map_france',
-            figure=Carte.get_map_graph(data, default_annee, default_departement),
-        )
-        ]),
-        # Camembert des faits les plus communs
-        html.Div(id='div_faits_les_plus_communs', children=[
-            html.Div(id='most_common_crimes_options', children=[
-                dcc.Dropdown(
-                    id='mcc-month-dropdown',
-                    options=[{'label': month, 'value': month} for month in mois],
-                    value=default_mois
-                ),
-                dcc.Dropdown(
-                    id='mcc-year-dropdown',
-                    options=[{'label': year, 'value': year} for year in annees],
-                    value=default_annee
-                ),
-                dcc.Dropdown(
-                    id='mcc-departement-dropdown',
-                    options=[{'label': departement, 'value': departement} for departement in departements],
-                    value=default_departement
-                )
-            ])
-            ,
-            dcc.Graph(
-                id='most_common_crimes',
-                figure=CamembertFaits.get_common_crimes_pie_graph(data, default_annee, default_mois, default_departement, ascending=False),
-            )]),
-            html.Div(id='div_faits_les_moins_communs', children=[
-                html.Div(id='least_common_crimes_options', children=[
+            # Header de la page
+            html.Header(children=[
+            html.H1(children='Dashboard ', className='center'),
+            html.H2(children='des délits et crimes en France', className='center'),
+            ]),
+            
+            # Contenu principal (Main)
+            
+            html.Main(children=[ 
+            
+            
+            # Section de la carte de la France
+            html.Section(id="sec_map_france",children=[
+            
+            # Options de la carte de la France
+                html.Div(id='sec_map_france_options', children=[
                     dcc.Dropdown(
-                        id='lcc-month-dropdown',
+                        id='map_year_dropdown',
+                        options=[{'label': year, 'value': year} for year in annees],
+                        value=default_annee
+                    ),
+                    dcc.Dropdown(
+                        id='map_month_dropdown',
+                        options=[{'label': month, 'value': month} for month in mois],
+                        value=default_mois
+                    ),
+                    ]),
+                
+                # Carte de la France
+                html.Div(id='sec_map_france_content', children=[
+                    dcc.Graph(
+                        id='map_france',
+                        figure=Carte.get_map_graph(data, default_annee, default_departement),
+                    )
+                    ]),
+            ]),
+            # Camembert des faits les plus communs
+            html.Section(id='sec_faits_les_plus_communs', children=[
+                
+                # Options du camembert des faits les plus communs
+                html.Div(id='most_common_crimes_options', children=[
+                    dcc.Dropdown(
+                        id='mcc-month-dropdown',
                         options=[{'label': month, 'value': month} for month in mois],
                         value=default_mois
                     ),
                     dcc.Dropdown(
-                        id='lcc-year-dropdown',
+                        id='mcc-year-dropdown',
                         options=[{'label': year, 'value': year} for year in annees],
                         value=default_annee
                     ),
                     dcc.Dropdown(
-                        id='lcc-departement-dropdown',
+                        id='mcc-departement-dropdown',
                         options=[{'label': departement, 'value': departement} for departement in departements],
                         value=default_departement
                     )
-            ]),
-            dcc.Graph(
-                id='least_common_crimes',
-                figure=CamembertFaits.get_common_crimes_pie_graph(data, default_annee, default_mois, default_departement, ascending=True),
-            )]),
-            html.Div(id='div_histogramme_par_mois', children=[
-                html.Div(id='histogramme_par_mois_options', children=[
-                    dcc.Dropdown(
-                        id='hpm-year-dropdown',
-                        options=[{'label': year, 'value': year} for year in annees],
-                        value=default_annee
-                    ),
-                    dcc.Dropdown(
-                        id='hpm-departement-dropdown',
-                        options=[{'label': departement, 'value': departement} for departement in departements],
-                        value=default_departement,
-                        multi=True
-                    ),
-                    
-                    dcc.Dropdown(
-                        id='hpm-fait-dropdown',
-                        options=[{'label': fait, 'value': fait} for fait in faits],
-                        value=default_fait
-                    )
                 ]),
-                dcc.Graph(
-                    id='histogramme_par_mois',
-                    figure=HistogrammeParMois.get_histogramme_graph(data, default_annee, default_departement, 'Tout'),
-                )
-            ]),
+                
+                # Camembert des faits les plus communs
+                html.Div(id='sec_faits_les_plus_communs_content', children=[
+                    dcc.Graph(
+                        id='most_common_crimes',
+                        figure=CamembertFaits.get_common_crimes_pie_graph(data, default_annee, default_mois, default_departement, ascending=False),
+                    )
+                    ]),
+                ]),
+            
+                # Section du camembert des faits les moins communs
+                html.Section(id='sec_faits_les_moins_communs', children=[
+                    html.Div(id='least_common_crimes_options', children=[
+                        dcc.Dropdown(
+                            id='lcc-month-dropdown',
+                            options=[{'label': month, 'value': month} for month in mois],
+                            value=default_mois
+                        ),
+                        dcc.Dropdown(
+                            id='lcc-year-dropdown',
+                            options=[{'label': year, 'value': year} for year in annees],
+                            value=default_annee
+                        ),
+                        dcc.Dropdown(
+                            id='lcc-departement-dropdown',
+                            options=[{'label': departement, 'value': departement} for departement in departements],
+                            value=default_departement
+                        )
+                ]),
+                    # Camembert des faits les moins communs
+                    html.Div(id='sec_faits_les_moins_communs_content', children=[
+                        dcc.Graph(
+                            id='least_common_crimes',
+                            figure=CamembertFaits.get_common_crimes_pie_graph(data, default_annee, default_mois, default_departement, ascending=True),
+                        )
+                        ]),
+                    ]),
+                # Section de l'histogramme par mois
+                html.Section(id='sec_histogramme_par_mois', children=[
+                    # Options de l'histogramme par mois
+                    html.Div(id='histogramme_par_mois_options', children=[
+                        dcc.Dropdown(
+                            id='hpm-year-dropdown',
+                            options=[{'label': year, 'value': year} for year in annees],
+                            value=default_annee
+                        ),
+                        dcc.Dropdown(
+                            id='hpm-departement-dropdown',
+                            options=[{'label': departement, 'value': departement} for departement in departements],
+                            value=default_departement,
+                            multi=True
+                        ),
+                        
+                        dcc.Dropdown(
+                            id='hpm-fait-dropdown',
+                            options=[{'label': fait, 'value': fait} for fait in faits],
+                            value=default_fait
+                        )
+                    ]),
+                    # Histogramme par mois
+                    html.Div(id='sec_histogramme_par_mois_content', children=[
+                        dcc.Graph(
+                            id='histogramme_par_mois',
+                            figure=HistogrammeParMois.get_histogramme_graph(data, default_annee, default_departement, 'Tout'),
+                        )
+                        ]),
+                    ]),
+                ]),
         ])
 
 
